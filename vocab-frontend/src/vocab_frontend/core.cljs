d26ba56ebbdbc506c24854c9dc03691785f407bb
rvalentini <riccardo.valentini@comsysto.com>
2019-03-15 16:56:57 +0100

    moved map to separate component and played around with atom state handling

--- vocab-frontend/src/vocab_frontend/core.cljs
+++ vocab-frontend/src/vocab_frontend/core.cljs
@@ -1,6 +1,6 @@
 (ns vocab-frontend.core
   (:require [reagent.core :as reagent]
-            [vocab-frontend.map :as leaflet]))
+            [vocab-frontend.map :as map]))
 
 ;;TODO are externs actually necessary/working here?
 ;;TODO how to interact with the leaflet map?
@@ -15,18 +15,8 @@
   (swap! app-state assoc :message "You clicked me :P"))
 
 
-(defn leaflet-init []
-  (set! (.-accessToken js/mapboxgl) "pk.eyJ1Ijoicml2YWwiLCJhIjoiY2lxdWxpdHRqMDA0YWk3bTM1Mjc1dmVvYiJ9.uxBDzgwojTzU-Orq2AEUZA")
-  (new js/mapboxgl.Map (clj->js {:container "map"
-                                 :style "mapbox://styles/rival/cjt705zrp0j781gn20szdi3y1"
-                                 :center [-122.403944, 37.784020],
-                                 :zoom 10.6})))
-
-(defn leaflet-render []
-  [:div#map])
-
 (defn app []
-  (reagent/create-class {:reagent-render      leaflet-render
-                         :component-did-mount leaflet-init}))
+  [map/map-component])
+
 
 (reagent/render [app] (js/document.querySelector "#cljs-target"))
--- vocab-frontend/src/vocab_frontend/map.cljs
+++ vocab-frontend/src/vocab_frontend/map.cljs
@@ -2,7 +2,25 @@
   (:require [reagent.core :as reagent]))
 
 
-;;TODO leaflet should be contained in this component
+(def map (reagent/atom {:map nil} ))                        ;;TODO nested is not good
+
+(defn- map-init []
+  (set! (.-accessToken js/mapboxgl) "pk.eyJ1Ijoicml2YWwiLCJhIjoiY2lxdWxpdHRqMDA0YWk3bTM1Mjc1dmVvYiJ9.uxBDzgwojTzU-Orq2AEUZA")
+  (swap! map assoc :map (new js/mapboxgl.Map (clj->js {:container "map"
+                                            :style "mapbox://styles/rival/cjt705zrp0j781gn20szdi3y1"
+                                            :center [-122.403944, 37.784020],
+                                            :zoom 10.6})))
+    (.addControl (:map @map) (new js/mapboxgl.NavigationControl)))
+
+
+(defn- map-render []
+  [:div#map])
+
+(defn map-component []
+  (reagent/create-class {:reagent-render      map-render
+                         :component-did-mount map-init}))
+
+
 
 
 
